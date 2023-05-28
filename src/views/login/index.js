d407719851779f6c7711df6f9c8eafd9934f96dd
Graziellle127 <grazigrazielle454@gmail.com>
2023-05-28 16:09:32 -0300

    personalizando a view de login

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import Style from './styles.js'
 
-import { Container, View, Title } from "react-native";
+import { View, Text, TextInput, Button } from 'react-native';
+
+import {
+    Container, InputArea, CustomButton, CustomButtonText,
+    SignMessageButton, SignMessageButtonText, SignMessageButtonTextBold, Logo
+} from './styles';
+
+// componentes
+import ViewFood from '../../components/icons/ViewFood.js';
+import SignInput from '../../components/SignInput';
 
 // navegacao
 import { useNavigation } from "@react-navigation/native";
@@ -9,14 +19,55 @@ import { useNavigation } from "@react-navigation/native";
 
 const Login = () => {
 
+    const [usuario, setUsuario] = useState('');
+    const [senha, setSenha] = useState('');
+
     const navigation = useNavigation();
 
-    return (
-        <View>
-            Login
-        </View>
+    const handleLoginClick = () => {
+        // autenticação
+        console.log('Usuário:', usuario);
+        console.log('Senha:', senha);
+    };
 
-    )
+    const handleCadastroClick = () => {
+        // Navegar para a tela de cadastro
+        console.log('Ir para a tela de cadastro');
+        navigation.navigate('CadastroUsuario')
+    };
+
+    return (
+        <Container>
+            <Logo>
+                <ViewFood />
+            </Logo>
+            <InputArea>
+                <SignInput
+                    icon='mail'
+                    placeholder='Digite seu e-mail'
+                    value={usuario}
+                    onChangeText={t => setUsuario(t)}
+                />
+                <SignInput
+                    icon='lock'
+                    placeholder='Digite sua senha'
+                    value={senha}
+                    onChangeText={t => setSenha(t)}
+                    password={true}
+                />
+                <CustomButton onPress={handleLoginClick}>
+                    <CustomButtonText>Login</CustomButtonText>
+                </CustomButton>
+            </InputArea>
+
+            <SignMessageButton onPress={handleCadastroClick}>
+                <SignMessageButtonText>Ainda não possui uma conta?</SignMessageButtonText>
+                <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
+            </SignMessageButton>
+
+
+        </Container>
+    );
 }
 
 export default Login;
---
+++ src/views/login/styles.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+
+export const Container = styled.SafeAreaView`
+background-color: #FE6734;
+flex: 1;
+justify-content: center;
+align-items: center;
+`
+
+export const InputArea = styled.View`
+padding: 40px;
+width: 100%;
+`
+
+export const Logo = styled.View`
+transform: scale(0.8)
+`
+
+export const CustomButton = styled.TouchableOpacity`
+height: 60px;
+background-color: #DA5C31;
+border-radius: 30px;
+justify-content: center;
+align-items: center;
+`
+
+export const CustomButtonText = styled.Text`
+font-size: 20px;
+color: #FFF;
+`
+
+export const SignMessageButton = styled.TouchableOpacity`
+flex-direction: row;
+justify-content: center;
+margin-top: 20px;
+margin-bottom: 50px;
+`
+
+export const SignMessageButtonText = styled.Text`
+font-size: 14px;
+color: #4A4A4A;
+`
+
+export const SignMessageButtonTextBold = styled.Text`
+font-size: 14px;
+color: #fff;
+font-weight: bold;
+margin-left: 5px;
+margin-bottom: 50px;
+`
